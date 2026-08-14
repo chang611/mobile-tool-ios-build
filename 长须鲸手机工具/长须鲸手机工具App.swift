4a7c938c4d62b48d1e7363f6c044bfb249e96708
chang611 <Tunshiyiqie@outlook.com>
2026-08-14 19:49:01 +0800

    ?? ???????/???????App.swift

---
+++ 长须鲸手机工具/长须鲸手机工具App.swift
@@ -0,0 +1,17 @@
+//
+//  长须鲸手机工具App.swift
+//  长须鲸手机工具
+//
+//  Created by 长须鲸 on 2026/8/14.
+//
+
+import SwiftUI
+
+@main
+struct 长须鲸手机工具App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
